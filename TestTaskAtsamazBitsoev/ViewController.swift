0371a7dd8fc34256ab8b560f9ee8243dc3b50292
Atsamaz <atsamazbitsoev@yandex.ru>
2019-02-16 15:26:41 +0300

    Initial Commit

---
+++ TestTaskAtsamazBitsoev/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestTaskAtsamazBitsoev
+//
+//  Created by Ацамаз on 16/02/2019.
+//  Copyright © 2019 a.s.bitsoev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
